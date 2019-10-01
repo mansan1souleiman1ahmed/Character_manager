df62cc17f2f6c91cbe0fbc1da6bf4e2556a32900
mansan1souleiman1ahmed <mansan.souleiman.ahmed@gmail.com>
2019-10-01 16:48:46 +0200

    requete bac-end à faire

--- server/index.js
+++ server/index.js
@@ -26,3 +26,48 @@ axios
     });
     document.getElementById("list-display").innerHTML = output;
   });
+//Function pour faire des post
+let nameInput = document.getElementById("name");
+let shortInput = document.getElementById("shortDesc");
+let descInput = document.getElementById("desc");
+const createForm = document.getElementById("create");
+
+createForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  postUser();
+});
+
+async function postUser() {
+  try {
+    const response = await axios
+      .post("https://character-database.becode.xyz/characters", {
+        name: nameInput.value,
+        shortDescription: shortInput.value,
+        description: descInput.value
+        //   image: 'https://source.unsplash.com/random'
+      })
+      .then(response => {
+        console.log(response);
+        return response;
+      });
+  } catch (error) {
+    console.error(error);
+  }
+}
+//Function qui transforme les images en base 64
+/* function readFile(el) {
+  if (el.files && el.files[0]) {
+    var FR = new FileReader();
+
+    FR.addEventListener("load", function(e) {
+      console.log(e.target.result);
+    });
+
+    FR.readAsDataURL(el.files[0]);
+  }
+}
+
+document.querySelector("#files").addEventListener("change", el => {
+  readFile(el.currentTarget);
+});
+ */
